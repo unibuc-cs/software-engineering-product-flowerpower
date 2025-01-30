757ba1adcca9e1f9e1e260ae92229612717f3a77
Vizinteanu Teodora <115303402+TeoVizi@users.noreply.github.com>
2025-01-30 22:35:19 +0200

    stilizare friends list

--- frontend/src/app/search-user/search-user.component.ts
+++ frontend/src/app/search-user/search-user.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-search-user',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, MatButton],
   templateUrl: './search-user.component.html',
   styleUrls: ['./search-user.component.css']
 })
@@ -14,6 +15,7 @@ export class SearchUserComponent implements OnInit {
   users: any[] = [];
   searchQuery: string = '';
   senderId: number;
+  isSearchClicked: boolean = false;
 
   constructor(private userService: UserService) {
     const userId = sessionStorage.getItem('userId');
@@ -22,18 +24,25 @@ export class SearchUserComponent implements OnInit {
 
   ngOnInit(): void {}
 
+
   searchUsers(): void {
+    this.isSearchClicked = true; 
+
     if (this.searchQuery) {
       this.userService.searchUsers(this.searchQuery).subscribe(
-        (data: any) => {
-          this.users = data;
-        },
-        (error: any) => {
-          console.error('Error searching users', error);
-        }
+          (data: any) => {
+            this.users = data;
+          },
+          (error: any) => {
+            console.error('Error searching users', error);
+            this.users = []; 
+          }
       );
+    } else {
+      this.users = []; 
     }
   }
+
 
   sendFriendRequest(receiverId: number): void {
     this.userService.sendFriendRequest(this.senderId, receiverId).subscribe(
